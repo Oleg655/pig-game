297728a1b43747d38d561ecb5cde7d51e4c0bbb1
Oleg655 <savinoleg95@gmail.com>
2022-11-15 16:54:59 +0300

    feat: create condition for players

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 'use strict';
 
+const player0EL = document.querySelector('.player--0')
+const player1EL = document.querySelector('.player--1')
+
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
-const diceEL = document.querySelector('.dice')
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0EL.textContent = 0;
 score1EL.textContent = 0;
-diceEL.classList.add('hidden')
+diceEL.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+		player0EL.classList.toggle('player--active');
+		player1EL.classList.toggle('player--active');
+  }
+});
